5565958293f6905f00138c104589b82d29ffd26d
vijay <vijaysm97@gmail.com>
2020-02-25 20:43:25 +0530

    testing card

---
+++ widget/intro_card/IntroCard.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+// import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native';
+import "./introCard.css"
+
+export default function IntroCard() {
+    return (
+        <View className="card">
+            <Text >Hello I'm vijay</Text>
+        </View>
+    );
+}
